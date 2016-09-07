18b37cabc72a51b0eaf9ed1ea3c1e2c964e3fd02
Thomas N Rizzo III <thomasnrizzoiii@Thomass-MacBook-Pro.local>
2016-09-07 11:15:58 -0600

    fourth commit

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'This application is used for testing Mocha and Chai testing' });
+  res.render('index', { title: 'This application is used for Mocha and Chai testing' });
 });
 
 module.exports = router;
